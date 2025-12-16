83c07ce26dddec2ac30e899f7186bd6c6ca0ffc6
Giovanni Vásquez <146869565+GiovaniVs@users.noreply.github.com>
2025-12-15 22:13:04 -0600

    agregue logo y el dropdow

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const menu = document.getElementById('menu');
 const sidevar = document.getElementById('sidevar');
 const menuIcon = menu.firstElementChild;
 const main = document.getElementById('main'); 
+const userBtn = document.getElementById('userBtn');
+const dropdown = document.querySelector('.dropdown-menu');
 
+console.log(userBtn); 
+console.log(dropdown);
 menu.addEventListener('click', () => {
     sidevar.classList.toggle('menu-toggle');
     main.classList.toggle('menu-toggle'); 
@@ -12,3 +16,8 @@ menu.addEventListener('click', () => {
         menuIcon.textContent = 'menu';
     }
 });
+
+userBtn.addEventListener('click', (e) => {
+     e.preventDefault(); // evita salto del <a>
+    dropdown.classList.toggle('active');
+}); 
